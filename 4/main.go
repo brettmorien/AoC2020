ac4f4dde4925e940dc1cb0f7dd9b1a9fa5b9de87
Brett Morien <brett@facetdev.com>
2020-12-13 13:07:31 -0800

    Add bonus day 4 validation code. Complete.

--- 4/main.go
+++ 4/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -18,16 +20,78 @@ type record struct {
 }
 
 func (r *record) validate() bool {
-	return len(r.byr) > 0 &&
-		len(r.iyr) > 0 &&
-		len(r.eyr) > 0 &&
-		len(r.hgt) > 0 &&
-		len(r.hcl) > 0 &&
-		len(r.ecl) > 0 &&
-		len(r.pid) > 0
+	// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	// hgt (Height) - a number followed by either cm or in:
+	// If cm, the number must be at least 150 and at most 193.
+	// If in, the number must be at least 59 and at most 76.
+	// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+	// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+	// pid (Passport ID) - a nine-digit number, including leading zeroes.
+	// cid (Country ID) - ignored, missing or not.
+
+	if !validateYear(r.byr, 1920, 2002) {
+		return false
+	}
+	if !validateYear(r.iyr, 2010, 2020) {
+		return false
+	}
+	if !validateYear(r.eyr, 2020, 2030) {
+		return false
+	}
+
+	if strings.HasSuffix(r.hgt, "cm") {
+		hgt, err := strconv.Atoi(r.hgt[:len(r.hgt)-2])
+		if err != nil || hgt < 150 || hgt > 193 {
+			return false
+		}
+	} else if strings.HasSuffix(r.hgt, "in") {
+		hgt, err := strconv.Atoi(r.hgt[:len(r.hgt)-2])
+		if err != nil || hgt < 59 || hgt > 76 {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	if match, _ := regexp.MatchString("^#[a-f0-9]{6}$", r.hcl); !match {
+		return false
+	}
+
+	eyeColors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+	colorMatch := false
+	for _, color := range eyeColors {
+		if r.ecl == color {
+			colorMatch = true
+		}
+	}
+	if !colorMatch {
+		return false
+	}
+
+	if match, _ := regexp.MatchString("^[0-9]{9}$", r.pid); !match {
+		return false
+	}
+
+	return true
+}
+
+func validateYear(yearString string, min int, max int) bool {
+	if len(yearString) != 4 {
+		return false
+	}
+	year, err := strconv.Atoi(yearString)
+
+	if err != nil || year < min || year > max {
+		return false
+	}
+
+	return true
 }
 
 func main() {
+	// data := readData("sample2.data")
 	data := readData("official.data")
 
 	records := splitRecords(data)
@@ -36,10 +100,11 @@ func main() {
 	for _, record := range records {
 		if record.validate() {
 			valid++
+			// fmt.Printf("%v\n", record)
 		}
 	}
 
-	fmt.Printf("Valid passports: %v\n", valid)
+	fmt.Printf("Valid passports: %v/%v\n", valid, len(records))
 }
 
 func splitRecords(data []string) []record {
@@ -53,13 +118,10 @@ func splitRecords(data []string) []record {
 }
 
 func parseRecord(data string) record {
-	tokens := strings.Fields(data)
-
 	values := map[string]string{}
 
-	for _, token := range tokens {
+	for _, token := range strings.Fields(data) {
 		parts := strings.Split(token, ":")
-
 		values[parts[0]] = parts[1]
 	}
 
