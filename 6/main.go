03de8ad692d6b15c0f3b9ba7c065ca521010ce5a
Brett Morien <brett@facetdev.com>
2020-12-15 21:22:14 -0800

    Implementation part 2 of day 6.

--- 6/main.go
+++ 6/main.go
@@ -3,22 +3,58 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
+	policyType := "any"
+	if len(os.Args) > 1 {
+		policyType = os.Args[1]
+	}
+
+	if policyType != "any" && policyType != "all" {
+		panic("Unsupported answer policy type. Use \"any\" or \"all\"")
+	}
+
 	records := readRecords("official.data")
 
 	total := 0
+
+	getAnswers := getAnyYesAnswers
+	if policyType == "all" {
+		getAnswers = getAllYesAnswers
+	}
+
 	for _, record := range records {
-		answers := getYesAnswers(record)
+		answers := getAnswers(record)
 		total += answers
 	}
 	fmt.Println("Total answers", total)
 }
 
-func getYesAnswers(record string) int {
+func getAllYesAnswers(record string) int {
+	values := map[string]bool{}
+
+	m1 := regexp.MustCompile(`[^a-z]`)
+	allAnswers := m1.ReplaceAllString(record, "")
+
+	responses := strings.Split(record, "\n")
+	for _, a := range allAnswers {
+		all := true
+		for _, r := range responses {
+			all = all && (strings.IndexRune(r, a) != -1)
+		}
+		if all {
+			values[string(a)] = true
+		}
+	}
+
+	return len(values)
+}
+
+func getAnyYesAnswers(record string) int {
 	values := map[string]bool{}
 
 	m1 := regexp.MustCompile(`[^a-z]`)
