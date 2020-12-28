61cd34a6a16cf0c29aa2dfd481320f12f79796ca
Brett Morien <brett@facetdev.com>
2020-12-27 21:34:06 -0800

    Implement day 9 bonus

--- 9/main.go
+++ 9/main.go
@@ -11,8 +11,17 @@ const debug = false
 
 func main() {
 	data := readData("official.data")
-	windowSize := 25
 
+	weakness := findWeakness(data, 25)
+	fmt.Printf("Weakness: %v\n", weakness)
+
+	set := findContiguous(weakness, data)
+
+	encWeakness := min(set) + max(set)
+	fmt.Printf("Weakness: %v - Found set: %v\n", encWeakness, set)
+}
+
+func findWeakness(data []int, windowSize int) int {
 	window := make([]int, windowSize)
 
 	for i := 0; i < windowSize; i++ {
@@ -22,11 +31,57 @@ func main() {
 	for i, val := range data[windowSize:] {
 		valid := validate(val, window)
 		if !valid {
-			fmt.Printf("Not valid: %v\n", val)
+			return val
 		}
 		window[i%windowSize] = val
 	}
+	return 0
+}
 
+func findContiguous(total int, data []int) []int {
+	for begin := range data {
+		for end := range data[begin+2:] {
+			sum := 0
+			for _, i := range data[begin : end+begin] {
+				sum += i
+			}
+			if sum == total {
+				return data[begin : end+begin]
+			}
+			if sum > total {
+				break
+			}
+		}
+	}
+	return []int{}
+}
+
+func min(items []int) int {
+	if len(items) == 0 {
+		return 0
+	}
+	min := items[0]
+
+	for _, i := range items {
+		if i < min {
+			min = i
+		}
+	}
+	return min
+}
+
+func max(items []int) int {
+	if len(items) == 0 {
+		return 0
+	}
+	max := items[0]
+
+	for _, i := range items {
+		if i > max {
+			max = i
+		}
+	}
+	return max
 }
 
 func validate(number int, window []int) bool {
