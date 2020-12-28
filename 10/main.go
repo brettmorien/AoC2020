cde63704f9b82bad491351642e53f146af8bdf91
Brett Morien <brett@facetdev.com>
2020-12-27 23:16:12 -0800

    Better handle beginning 0 and ending 3-gap.

--- 10/main.go
+++ 10/main.go
@@ -22,8 +22,6 @@ func main() {
 
 func findGaps(data []int) {
 	gaps := map[int]int{}
-	gaps[3] = 1
-	gaps[data[0]]++
 
 	for i, val := range data[1:] {
 		gaps[val-data[i]]++
@@ -36,10 +34,11 @@ func findGaps(data []int) {
 func readData(filename string) []int {
 	dataLines := readDataLines(filename)
 
-	result := make([]int, len(dataLines))
+	result := make([]int, len(dataLines)+2)
 	for i, line := range dataLines {
 		result[i], _ = strconv.Atoi(line)
 	}
+	result[len(result)-1] = max(result) + 3
 	sort.Ints(result)
 	return result
 }
@@ -48,6 +47,20 @@ func readDataLines(filename string) []string {
 	fileBytes, error := ioutil.ReadFile(filename)
 	check(error)
 	return strings.Split(string(fileBytes), "\n")
+}
+
+func max(items []int) int {
+	if len(items) == 0 {
+		return 0
+	}
+	max := items[0]
+
+	for _, i := range items {
+		if i > max {
+			max = i
+		}
+	}
+	return max
 }
 
 func check(err error) {
